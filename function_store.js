6d4f3f2de53931c26459f9b288a44e96a3a03520
andyzhang <andyandyzhang@126.com>
2019-06-18 22:03:28 +0800

    update files

--- function_store.js
+++ function_store.js
@@ -1,1 +1,16 @@
 //常用函数汇总
+
+//xmlhttp
+function xmlCreat(){
+    var xmlhttp;
+    if(window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest;
+    }else{//code for IE5,IE6
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function(){
+        if(xmlhttp.readystate==4 && xmlhttp.status==200){
+            //code
+        }
+    }
+}
